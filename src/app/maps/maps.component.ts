a2999a1a32df3f066764cee2566622c7a0a8da71
WilberthGamboa <wgcampos15@gmail.com>
2023-11-24 22:57:50 -0600

    se genera el mapa

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import {Component } from '@angular/core';
-
+import {Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+// cloud api AIzaSyDal8gWT9yatkSmST52fylAj0QGtf51tD4
 @Component({
   selector: 'app-maps',
   standalone: true,
@@ -16,9 +17,41 @@ import {Component } from '@angular/core';
 
 })
 
-export class MapsComponent {
+export class MapsComponent implements OnInit  {
+  safeUrl: SafeResourceUrl; // Change the type to SafeResourceUrl
+  latitudCliente:number = 0
+  longitudCliente:number = -0
+  constructor(private sanitizer: DomSanitizer) {
+    const unsafeUrl = 'your-unsafe-url'; // Replace 'your-unsafe-url' with the actual URL
+    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
+  }
   dataMaps:any = []
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.latitudCliente = position.coords.latitude;
+          this.longitudCliente = position.coords.longitude;
+          console.log('Latitude: ' + position.coords.latitude);
+          console.log('Longitude: ' + position.coords.longitude);
+
+          // Puedes hacer lo que necesites con la ubicación aquí
+        },
+        (error) => {
+          console.error('Error getting location', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }
   async buscarInformacion(valor:string){
+    this.dataMaps=[]
+    valor = valor.trim()
     console.log(valor)
     const url = 'http://148.209.67.83/rest/items/find-by-metadata-field'
     console.log(url)
@@ -90,6 +123,16 @@ export class MapsComponent {
   } catch (error:any) {
     console.error('Error al realizar la petición:', error.message);
   }
+  }
+
+  generateUrl(latitud: string, longitud: string): SafeResourceUrl {
+    // Lógica para generar la URL en función del id
+    const apiKey = 'AIzaSyDal8gWT9yatkSmST52fylAj0QGtf51tD4'; // Replace with your Google Maps API key
+    const url = `https://www.google.com/maps/embed/v1/directions?key=${apiKey}&origin=${this.latitudCliente},${this.longitudCliente}&destination=${latitud},${longitud}&avoid=tolls|highways`;
+   
+
+    // Use the sanitizer to make the URL safe
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
  }
 
